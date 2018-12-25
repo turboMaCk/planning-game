2131bfe732753f02b95de44e2235e0a4d89779c2
Marek Fajkus <marek.faj@gmail.com>
2018-12-25 19:47:27 +0100

    build docker

--- client.nix
+++ client.nix
@@ -1,19 +1,20 @@
 { stdenv, elm, server }:
+
 stdenv.mkDerivation {
-    name = "agile-poker-client";
-    src = ./.;
+  name = "agile-poker-client";
+  src = ./.;
 
-    buildInputs = [ elm ];
+  buildInputs = [ elm ];
 
-    buildPhase = ''
-        export ELM_HOME=$(pwd)
-        mkdir $out
-        elm make client/Main.elm --output=app.js
-    '';
+  buildPhase = ''
+    export ELM_HOME=$(pwd)
+    mkdir $out
+    elm make client/Main.elm --output=app.js
+  '';
 
-    installPhase = ''
-        mkdir $out/public
-        cp public/* $out/public/*
-        cp app.js $out/public/
-    '';
+  installPhase = ''
+    mkdir $out/public
+    cp public/* $out/public/*
+    cp app.js $out/public/
+  '';
 }
--- default.nix
+++ default.nix
@@ -1,28 +1,50 @@
 let
   config = {
     packageOverrides = pkgs: rec {
-      haskellPackages = pkgs.haskellPackages.override {
-        overrides = haskellPackagesNew: haskellPackagesOld: rec {
-          agilePoker =
-            haskellPackagesNew.callPackage ./server.nix {
-              libiconv = pkgs.libiconv;
-              client = client;
-            };
+      client =
+        pkgs.callPackage ./client.nix {
+          inherit pkgs;
+          elm = pkgs.elmPackages.elm;
+          server = pkgs.haskellPackages.agilePoker;
         };
-      };
+
+      # Build docker container
+      # Using staticaly liked version
+      # This makes a huge difference in container size
+      #    - Dynamically linked build container: 664M
+      #    - Statically linked build container: 12M
+      docker-container =
+        pkgs.dockerTools.buildImage {
+          name = "agile-poker";
+          config.Cmd = [ "${haskellPackages.agilePoker-mini}/bin/agile-poker" ];
+        };
+
+      haskellPackages =
+        pkgs.haskellPackages.override {
+          overrides = haskellPackagesNew: haskellPackagesOld: rec {
+            agilePoker =
+              haskellPackagesNew.callPackage ./server.nix {
+                libiconv = pkgs.libiconv;
+                client = client;
+              };
+
+            # statically liked version (used for docker)
+            agilePoker-mini =
+              pkgs.haskell.lib.justStaticExecutables
+                (haskellPackagesNew.callPackage ./server.nix {
+                      libiconv = pkgs.libiconv;
+                      client = client;
+                });
+          };
+        };
     };
   };
 
   pkgs =
     import <nixpkgs> { inherit config; };
-
-  client =
-    pkgs.callPackage ./client.nix {
-      stdenv = pkgs.stdenv;
-      elm = pkgs.elmPackages.elm;
-      server = pkgs.haskellPackages.agilePoker;
-    };
 in
-rec { server = pkgs.haskellPackages.agilePoker;
-      client = client;
-    }
+rec {
+  server = pkgs.haskellPackages.agilePoker;
+  client = pkgs.client;
+  docker = pkgs.docker-container;
+}
