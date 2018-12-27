3837c9943a3820070a66a22abb07719e8638571c
Marek Fajkus <marek.faj@gmail.com>
2018-12-27 11:31:29 +0100

    nix shell fix

--- shell.nix
+++ shell.nix
@@ -1,10 +1,1 @@
-{ pkgs ? import <nixpkgs> { } }:
-(pkgs.stdenv.mkDerivation {
-  name = "agile-poker-shell";
-  buildInputs = with pkgs;
-    [ elmPackages.elm
-      haskellPackages.ghcid
-      (import ./default.nix).server
-    ];
-})
-
+(import ./default.nix).server.env
