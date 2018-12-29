aeba3f3f83d0990a76cd500eac7539587db3cb58
Marek Fajkus <marek.faj@gmail.com>
2018-12-29 21:55:40 +0100

    replace index.html by blaze

--- server/AgilePoker/Api/Middleware.hs
+++ server/AgilePoker/Api/Middleware.hs
@@ -6,9 +6,13 @@ import           Data.Maybe                    (fromMaybe)
 import           Network.HTTP.Types            (status200)
 import           Network.Wai                   (Middleware, Response,
                                                 rawPathInfo, requestHeaders,
-                                                responseFile)
+                                                responseLBS)
 import           Network.Wai.Middleware.Static (addBase, staticPolicy)
 import           Network.Wai.Parse             (parseHttpAccept)
+
+import           Text.Blaze.Html.Renderer.Utf8 (renderHtml)
+import           Text.Blaze.Html5              as Html
+import           Text.Blaze.Html5.Attributes   as Attrs
 
 
 indexMiddleware :: Middleware
@@ -24,9 +28,8 @@ indexMiddleware application request respond =
   where
     indexRes :: Response
     indexRes =
-      responseFile status200 headers fileName Nothing
+      responseLBS status200 headers $ renderHtml index
         where
-            fileName = "public/index.html"
             headers = [ ( "Content-Type", "text/html" )
                       , ( "Cache-Control", "public, max-age=86400" )
                       ]
@@ -40,3 +43,21 @@ publicMiddleware =
 staticMiddleware :: Middleware
 staticMiddleware =
   publicMiddleware . indexMiddleware
+
+
+index :: Html
+index =
+  docTypeHtml ! lang "en_EN" $ do
+    Html.head $ do
+      Html.title "Agile Poker"
+      Html.meta
+        ! Attrs.charset "UTF-8"
+      Html.link
+        ! Attrs.href "https://fonts.googleapis.com/css?family=Roboto+Slab:300,400,700"
+        ! Attrs.rel "stylesheet"
+
+    Html.body $ do
+      Html.script ""
+        ! Attrs.src "/app.js"
+      Html.script ""
+        ! Attrs.src "/init.js"
