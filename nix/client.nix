b1dc8b4456ff4afe7204ee9b9def41ea03a94103
Marek Fajkus <marek.faj@gmail.com>
2019-09-21 13:57:49 +0200

    add minification via uglify

--- nix/client.nix
+++ nix/client.nix
@@ -5,6 +5,8 @@
 with (import nixpkgs config);
 
 let
+  nodePackages = import ./node-composition.nix {};
+
   mkDerivation =
     { srcs ? ./elm-srcs.nix
     , src
@@ -29,8 +31,19 @@ let
         mkdir -p $out/share/doc
         cp -r public $out/public
         ${lib.concatStrings (map (module: ''
+          js=$out/"app.js"
+          min=$out/"public/app.js"
+
           echo "compiling ${elmfile module}"
-          elm make ${elmfile module} --output $out/public/app.js --docs $out/share/doc/${module}.json --optimize
+          elm make ${elmfile module} --output $js --docs $out/share/doc/${module}.json --optimize
+
+          ${nodePackages.uglify-js}/bin/uglifyjs $js \
+            --compress 'pure_funcs="F2,F3,F4,F5,F6,F7,F8,F9,A2,A3,A4,A5,A6,A7,A8,A9",pure_getters,keep_fargs=false,unsafe_comps,unsafe' \
+            | ${nodePackages.uglify-js}/bin/uglifyjs --mangle --output=$min
+
+          echo "Compiled size:$(cat $js | wc -c) bytes  ($js)"
+          echo "Minified size:$(cat $min | wc -c) bytes  ($min)"
+          echo "Gzipped size: $(cat $min | gzip -c | wc -c) bytes"
         '') targets)}
       '';
     };
