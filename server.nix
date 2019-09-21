7d3b61326b732d438815ccfb54be3d264275d50f
Marek Fajkus <marek.faj@gmail.com>
2019-09-21 12:23:15 +0200

    add dependecy

--- server.nix
+++ server.nix
@@ -1,20 +1,19 @@
-{ mkDerivation, aeson, base, bytestring, containers, cookie
-, http-types, mtl, random, servant, servant-server
-, servant-websockets, stdenv, text, wai, wai-extra
-, wai-middleware-static, warp, websockets
-, time, blaze-html, blaze-markup, zlib, ...
+{ mkDerivation, aeson, base, blaze-html, blaze-markup, bytestring
+, containers, cookie, gitrev, http-types, mtl, random, servant
+, servant-server, servant-websockets, stdenv, text, time, wai
+, wai-extra, wai-middleware-static, warp, websockets
 }:
 mkDerivation {
   pname = "planning-game";
-  version = "0.1.0.0";
+  version = "0.2.1";
   src = ./.;
   isLibrary = false;
   isExecutable = true;
   executableHaskellDepends = [
-    aeson base bytestring containers cookie http-types mtl random
-    servant servant-server servant-websockets text wai wai-extra
-    wai-middleware-static warp websockets time blaze-html blaze-markup
+    aeson base blaze-html blaze-markup bytestring containers cookie
+    gitrev http-types mtl random servant servant-server
+    servant-websockets text time wai wai-extra wai-middleware-static
+    warp websockets
   ];
-  executablePkgconfigDepends = [ zlib ];
   license = stdenv.lib.licenses.agpl3;
 }
