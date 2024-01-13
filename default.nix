55b5b74e5df5499c6177c5f03b1cd05042eee005
Marek Fajkus <marek.faj@gmail.com>
2024-01-13 20:13:32 +0100

    use builtin to fetch pinned nixpkgs

--- default.nix
+++ default.nix
@@ -1,10 +1,8 @@
 let
   nixpkgs =
     # updated 28/08/20
-    (import <nixpkgs> {}).fetchFromGitHub {
-      owner = "NixOS";
-      repo = "nixpkgs-channels";
-      rev = "c59ea8b8a0e7f927e7291c14ea6cd1bd3a16ff38";
+    builtins.fetchTarball {
+      url = "https://github.com/NisOS/nixpkgs-channels/archive/c59ea8b.tar.gz";
       sha256 = "1ak7jqx94fjhc68xh1lh35kh3w3ndbadprrb762qgvcfb8351x8v";
     };
 
